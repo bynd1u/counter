173e66ed1287e37f1acc37515d16829cb67bf90b
bynd1u <semenbindu@gmail.com>
2025-03-13 08:36:47 +0100

    initialize React app with Vite setup and basic counter component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Counter from './components/Counter'
+
+const  App = ()  => {
+
+  return (
+    <div>
+      <h1>Kounter</h1>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
